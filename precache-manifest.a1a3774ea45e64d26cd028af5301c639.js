486c5d3089913ebcb5739f228cff9d2ccfbe34c1
topashgun <topashgun@gmail.com>
2020-07-21 01:04:27 +0530

    Updates

---
+++ precache-manifest.a1a3774ea45e64d26cd028af5301c639.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10689f4c84eaab3e9464ad54746af71",
+    "url": "/diagnal/index.html"
+  },
+  {
+    "revision": "200e8d591234d83c8bb0",
+    "url": "/diagnal/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "94db540558b1881ee1de",
+    "url": "/diagnal/static/css/main.f5ecf34c.chunk.css"
+  },
+  {
+    "revision": "200e8d591234d83c8bb0",
+    "url": "/diagnal/static/js/2.3ae51cf0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/diagnal/static/js/2.3ae51cf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94db540558b1881ee1de",
+    "url": "/diagnal/static/js/main.ea12cb88.chunk.js"
+  },
+  {
+    "revision": "5e670eea8570642220ae",
+    "url": "/diagnal/static/js/runtime-main.bc0863a4.js"
+  },
+  {
+    "revision": "d5f2a7f493e94ed267d41620eef143d9",
+    "url": "/diagnal/static/media/poster1.d5f2a7f4.jpg"
+  },
+  {
+    "revision": "81ccf144677486906fcf751450652895",
+    "url": "/diagnal/static/media/poster2.81ccf144.jpg"
+  },
+  {
+    "revision": "e242f0372e7bdbf2e48aa5afefc72b97",
+    "url": "/diagnal/static/media/poster3.e242f037.jpg"
+  },
+  {
+    "revision": "56f097245c605a5d312f103dedb21bcb",
+    "url": "/diagnal/static/media/poster4.56f09724.jpg"
+  },
+  {
+    "revision": "5ef7f79461c7a8c0a95652b813fb4722",
+    "url": "/diagnal/static/media/poster5.5ef7f794.jpg"
+  },
+  {
+    "revision": "164b05a3cd770650dd30bc71f5407e99",
+    "url": "/diagnal/static/media/poster6.164b05a3.jpg"
+  },
+  {
+    "revision": "23ac494a92660204d6b8acbba20aa0e9",
+    "url": "/diagnal/static/media/poster7.23ac494a.jpg"
+  },
+  {
+    "revision": "ef89f612f6e3d684892d6525b7957ce4",
+    "url": "/diagnal/static/media/poster8.ef89f612.jpg"
+  },
+  {
+    "revision": "65d6d5002a2f98860f29d3c61599c5ac",
+    "url": "/diagnal/static/media/poster9.65d6d500.jpg"
+  }
+]);
