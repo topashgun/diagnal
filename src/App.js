042c6cb71555da20365d2bc7f48dca8587209dec
topashgun <topashgun@gmail.com>
2020-07-21 01:31:06 +0530

    Added new favicon. Made some changes to the lg layout.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './App.css'
+import Header from './components/header'
+import Body from './components/Body'
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      searchText: ""
+    }
+    this.updateSearchText = this.updateSearchText.bind(this);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  updateSearchText(event) {
+    this.setState({
+      searchText: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header updateSearchText={this.updateSearchText}></Header>
+        <Body searchText={this.state.searchText}></Body>
+      </div>
+    );
+  }
 }
 
-export default App;
